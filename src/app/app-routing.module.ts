71e62bd33bbcc4bf446bafcae1f604046bad9675
Maxime <auneau.maxime@gmail.com>
2023-06-07 10:58:32 +0200

    Map Integration Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DemoPageComponent } from './pages/demo/demo.component';
+import { MainPageComponent } from "./pages/main-page/main-page.component";
 
 const routes: Routes = [
-  { path: '', component: DemoPageComponent }
+  { path: '', component: MainPageComponent }
 ];
 
 @NgModule({
