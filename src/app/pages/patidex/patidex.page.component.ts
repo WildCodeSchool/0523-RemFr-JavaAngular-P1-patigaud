b1a76305cbc0230cae3076b9bdafd1b2d0ad4ccd
Kevin <kevinroig37@gmail.com>
2023-06-08 17:52:50 +0200

    fix route problem on patidex after merge

--- src/app/pages/patidex/patidex.page.component.ts
+++ src/app/pages/patidex/patidex.page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-patidex',
+  selector: 'app-patidex-page',
   templateUrl: './patidex.page.component.html',
   styleUrls: ['./patidex.page.component.scss']
 })
