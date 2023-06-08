2b4a9fce4ca77afac113adf0d1626e956d0bdfa6
Kevin <kevinroig37@gmail.com>
2023-06-08 17:13:10 +0200

    integration of API is ok

--- src/app/components/patidex/patidex.component.ts
+++ src/app/components/patidex/patidex.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//import { ApiGardenService } from 'src/app/services/api-gardens.service';
-import { HttpClient } from '@angular/common/http';
-import { Location } from 'src/app/location';
-import { firstValueFrom, lastValueFrom } from 'rxjs';
 import { ApiGardenService } from 'src/app/services/api-gardens.service';
 
 @Component({
@@ -15,18 +11,17 @@ export class PatidexComponent implements OnInit {
   constructor(private apiGardenService: ApiGardenService) { }
 
   locations: any
-  status = "loading"
+  status = "ready"
 
   getLocations() {
     this.apiGardenService.getGardenList()
-    .then(() => {
+   .then(() => {
       this.locations = this.apiGardenService.data;
-      console.log(this.locations);
       this.status = "ready"
     })
     .catch((error) => {
       console.error(error)
-    })   
+    })
   }
 
   ngOnInit(): void {
--- src/app/services/api-gardens.service.ts
+++ src/app/services/api-gardens.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { firstValueFrom, lastValueFrom } from 'rxjs';
 import { Location } from '../location';
 
 @Injectable({
@@ -11,34 +10,42 @@ export class ApiGardenService {
   constructor(private httpClient: HttpClient) { }
 
   locations: any
+  locationList: any
 
   public async getGardenList() {
-    await firstValueFrom(this.httpClient
-      .get("https://data.tours-metropole.fr/api/records/1.0/search/?dataset=espaces-verts-tours-metropole-val-de-loire&q=&facet=commune&facet=libelle&facet=structure&facet=adresse&facet=aire_en_m")
-    )
-      .then((value) => {
-        let locationList = value as any;
-        this.locations = locationList.records;
-      })
-      .then(() => {
-        this.creatingNewList()
-      })
-
+    return this.httpClient
+      .get("https://data.tours-metropole.fr/api/records/1.0/search/?dataset=espaces-verts-tours-metropole-val-de-loire&q=&rows=1000&facet=commune&facet=libelle&facet=structure&facet=adresse&facet=aire_en_m")
+      .subscribe((response) => {
+        this.creatingNewList(response)
+      }
+      )
   }
+
+
   data: any = []
-  creatingNewList() {
+  creatingNewList(response: any) {
     let i = 0;
+    this.locationList = response;
+    this.locations = this.locationList.records;
+
     this.locations.forEach((location: Location) => {
 
       location = new Location(
+        location.id = this.locations[i].recordid,
         location.address = this.locations[i].fields.adresse,
-        location.shape = this.locations[i].fields.geo_shape.coordinates,
+        location.geoPoint = this.locations[i].geometry.coordinates,
+        location.area = this.locations[i].fields.aire_en_m,
+        location.structure = this.locations[i].fields.structure,
+        location.postalCode = this.locations[i].fields.code_insee,
+        location.label = this.locations[i].fields.libelle,
+        location.city = this.locations[i].fields.commune,
+        location.shape = this.locations[i].fields.geo_shape?.coordinates,
       );
 
       this.data.push(location);
-  
+
       i++;
-    }); 
-      return this.data;
+    });
+    return this.data;
   }
 }
