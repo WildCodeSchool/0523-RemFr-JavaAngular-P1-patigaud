ddffa54cff7d88027ca12f43f2ab6a16e0dda2b0
Romain DESMIDT <rdesmidt47@gmail.com>
2023-06-23 14:57:37 +0200

    styling change when garden discovered, lastly detail will not open on undiscovered one

--- src/app/components/patidex/patidex.component.ts
+++ src/app/components/patidex/patidex.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiGardenService } from 'src/app/services/api-garden/api-gardens.service';
+import { Garden } from 'src/app/models/garden';
+import { GardenService } from "src/app/services/garden/garden.service";
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-patidex',
@@ -7,11 +10,13 @@ import { ApiGardenService } from 'src/app/services/api-garden/api-gardens.servic
   styleUrls: ['./patidex.component.scss']
 })
 export class PatidexComponent implements OnInit {
-  openedIndex: number = -1;
-  constructor(private apiGardenService: ApiGardenService) { }
+  openedIndex = -1;
+  constructor(private apiGardenService: ApiGardenService, private GardenService: GardenService,) { }
 
   locations: any
   status = "ready"
+  public myGardenIds: any = [];
+  private gardens: Garden[] = []; 
 
   getLocations() {
     this.apiGardenService.getGardenList()
@@ -28,5 +33,33 @@ export class PatidexComponent implements OnInit {
 
   ngOnInit(): void {
     this.getLocations();
+    this.getAllGardens();
   }
+
+  isInsideMyGardenIds(id: any): boolean {
+    if(this.myGardenIds.includes(id)) {
+      return true
+    }
+    return false
+  }
+
+  getAllGardens() {
+    this.GardenService.getAll().snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c =>
+            ({ key: c.payload.key, ...c.payload.val() })
+          )
+        )
+      ).subscribe((data: any) => {
+        this.gardens = data;
+        const currentUser = localStorage.getItem('pseudo')
+        for (let i = 0; i < this.gardens.length; i++) {
+          if (this.gardens[i].userKey == currentUser) {
+            this.myGardenIds.push(this.gardens[i].gardenId)
+          }
+        }
+        console.log(this.myGardenIds)
+      })    
+  }  
 }
